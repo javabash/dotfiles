1aa616fa70a17382e60ad623391cb07569770153
Philip GeLinas <4793466+javabash@users.noreply.github.com>
2021-11-07 11:59:24 -0800

    Added to script to install from dotbot.

---
+++ setup_homebrew.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Homebrew Setup >>>\n"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install httpie
+brew install bat
+brew install google-chrome
+brew install visual-studio-code
+
+
